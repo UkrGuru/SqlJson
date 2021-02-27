cd6fcc1d5f092f18ebdee717a927acfa72ffba3b
AV <avictor@live.com>
2021-02-27 14:27:08 +0200

    Added WebAppDemo

---
+++ src/WebAppDemo/WebAppDemo/Pages/Contacts/Create.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using UkrGuru.SqlJson;
+using WebAppDemo.Models;
+
+namespace WebAppDemo.Pages.Contacts
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DbService _db;
+
+        public CreateModel(DbService db)
+        {
+            _db = db;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Contact Contact { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _db.ExecProcAsync("Contacts_Ins", Contact);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
