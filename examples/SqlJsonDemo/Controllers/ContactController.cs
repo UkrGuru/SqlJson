20e3e32c52d872abd5539491528f96fd2e559a93
AV <avictor@live.com>
2021-10-04 21:25:40 +0300

    2021-10-04

--- examples/SqlJsonDemo/Controllers/ContactController.cs
+++ examples/SqlJsonDemo/Controllers/ContactController.cs
@@ -6,28 +6,28 @@ using UkrGuru.SqlJson;
 
 namespace SqlJsonDemo.Controllers
 {
-[ApiController]
-[Route("api/[controller]")]
-public class ContactController : ControllerBase
-{
-    private readonly DbService _db;
-    public ContactController(DbService db) => _db = db;
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactController : ControllerBase
+    {
+        private readonly DbService _db;
+        public ContactController(DbService db) => _db = db;
 
-    [HttpGet]
-    public async Task<List<Contact>> Get() => await _db.FromProcAsync<List<Contact>>("Contacts_List");
+        [HttpGet]
+        public async Task<List<Contact>> Get() => await _db.FromProcAsync<List<Contact>>("Contacts_List");
 
-    [HttpGet("{id}")]
-    public async Task<Contact> Get(int id) => await _db.FromProcAsync<Contact>("Contacts_Item", new { Id = id });
+        [HttpGet("{id}")]
+        public async Task<Contact> Get(int id) => await _db.FromProcAsync<Contact>("Contacts_Item", new { Id = id });
 
-    [HttpPost]
-    public async Task<int> Post([FromBody] Contact item) => await _db.FromProcAsync<int>("Contacts_Ins", item);
+        [HttpPost]
+        public async Task<int> Post([FromBody] Contact item) => await _db.FromProcAsync<int>("Contacts_Ins", item);
 
-    [HttpPut("{id}")]
-    public async Task Put(int id, [FromBody] Contact item) => await _db.ExecProcAsync("Contacts_Upd", item);
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] Contact item) => await _db.ExecProcAsync("Contacts_Upd", item);
 
-    [HttpDelete("{id}")]
-    public async Task Delete(int id) => await _db.ExecProcAsync("Contacts_Del", new { Id = id });
-}
+        [HttpDelete("{id}")]
+        public async Task Delete(int id) => await _db.ExecProcAsync("Contacts_Del", new { Id = id });
+
         // DbHelper Demo
         // POST api/<ContactController>/PostGet
         [HttpPost("PostGet")]
