d5aa13de7b6f83fabbfe1721b0b987007b9b8cff
AV <avictor@live.com>
2021-10-26 17:37:42 +0300

    3.0.2

--- src/UkrGuru.SqlJson/Extensions/ServiceCollectionExtensions.cs
+++ src/UkrGuru.SqlJson/Extensions/ServiceCollectionExtensions.cs
@@ -1,10 +1,18 @@
-﻿using UkrGuru.SqlJson;
+﻿// Copyright (c) Oleksandr Viktor (UkrGuru). All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using UkrGuru.SqlJson;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class SqlJsonServiceCollectionExtensions
     {
-        public static void AddSqlJson(this IServiceCollection services, string connString = null)
+        public static void AddSqlJson(this IServiceCollection services)
+        {
+            services.AddScoped<DbService>();
+        }
+
+        public static void AddSqlJson(this IServiceCollection services, string connString)
         {
             DbHelper.ConnString = connString;
             services.AddSqlJson();
