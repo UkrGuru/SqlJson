459dfd6abf763c27866fe08172fb611889dac57b
A V <alexander@businessnetsolutions.co.uk>
2023-10-26 19:19:38 +0300

    EF7BulkUpdateBenchmark

---
+++ benchmark/EF7BulkUpdateBenchmark/BloggingContext.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class MyContext : DbContext
+{
+    private int _batchSize;
+
+    public MyContext(int batchSize = 42)
+    {
+        _batchSize = batchSize;
+    }
+
+    public DbSet<Blog> Blogs { get; set; }
+
+    public DbSet<RssBlog> RssBlogs { get; set; }
+
+    public DbSet<PrivateBlog> PrivateBlogs { get; set; }
+
+    public DbSet<PrivateBlogWithMFA> PrivateBlogsWithMFA { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+                => optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EF7BulkUpdate;Trusted_Connection=True", opt => opt.MaxBatchSize(_batchSize));
+    //.EnableSensitiveDataLogging()
+    //    .LogTo(
+    //        s =>  Console.WriteLine(s), LogLevel.Information);
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string? Url { get; set; }
+    public bool? IsActive { get; set; }
+    public DateTime LastUpdated { get; set; }
+}
+
+public class RssBlog : Blog
+{
+    public string RssUrl { get; set; }
+}
+
+public class PrivateBlog : Blog
+{
+    public string Password { get; set; }
+}
+
+public class PrivateBlogWithMFA : PrivateBlog
+{
+    public bool mfaOn { get; set; }
+}
---
+++ benchmark/EF7BulkUpdateBenchmark/EF7BulkUpdateBenchmark.cs
@@ -0,0 +1,111 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Reports;
+using Microsoft.EntityFrameworkCore;
+using UkrGuru.SqlJson;
+
+namespace BenchmarkDotNet.Samples
+{
+    [Config(typeof(Config))]
+    [SimpleJob(RuntimeMoniker.Net70)]
+    [MemoryDiagnoser]
+    [HideColumns(Column.AllocRatio, Column.RatioSD, Column.Median, Column.Gen0, Column.Gen1)]
+    public class EF7BulkUpdateBenchmark
+    {
+        private string SQLConnectionString
+            = "Data Source=(localdb)\\mssqllocaldb;Database=EF7BulkUpdate;Integrated Security=sspi;";
+
+        [Params(42, 500, 1000)]
+        public int records;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            using var context = new MyContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var random = new Random();
+            var date = DateTime.UtcNow;
+
+            for (int i = 0; i < records; i++)
+            {
+                context.Add(new
+                    Blog
+                {
+                    Url = "https://blogurl/ID/" + random.Next(1, 10000).ToString(),
+                    LastUpdated = date
+                });
+            }
+
+            context.SaveChanges();
+            
+            DbHelper.ConnectionString = SQLConnectionString;
+        }
+
+        [Benchmark]
+        public void UkrGuru_SqlJson_SqlUpdate() => DbHelper.Exec("UPDATE [Blogs] SET [LastUpdated] = @Data;", DateTime.UtcNow);
+
+        [Benchmark]
+        public void UkrGuru_SqlJson_NetUpdate()
+        {
+            var date = DateTime.UtcNow;
+
+            var blogs = DbHelper.Exec<List<Blog>>("SELECT [BlogId], [LastUpdated] FROM [Blogs] FOR JSON PATH") ?? new();
+
+            DbHelper.Exec("""
+                UPDATE [Blogs]
+                SET [LastUpdated] = D.[LastUpdated]
+                FROM OPENJSON(@Data)
+                    WITH([BlogId] int, [LastUpdated] datetime2(7)) D
+                WHERE [Blogs].[BlogId] = D.[BlogId]
+                """, blogs.Select(blog => { blog.LastUpdated = date; return new { blog.BlogId, blog.LastUpdated }; }));
+        }
+
+        [Benchmark]
+        public void EF_PlainSQL_BulkUpdate()
+        {
+            using var context = new MyContext();
+
+            context.Database
+                .ExecuteSql($"UPDATE [Blogs] SET [LastUpdated] = {DateTime.UtcNow}; --PlainSQLUpdate");
+        }
+
+        [Benchmark(Baseline = true)]
+        public void EF_ExecuteUpdate_BulkUpdate()
+        {
+            using var context = new MyContext();
+
+            context.Blogs.ExecuteUpdate(
+                s => s.SetProperty(
+                    blog => blog.LastUpdated, DateTime.UtcNow));
+        }
+
+        [Benchmark]
+        public void EF_SaveChanges_NoBulkUpdate()
+        {
+            using var context = new MyContext();
+            var date = DateTime.UtcNow;
+
+            var blogs = context.Blogs.ToList();
+            foreach (var blog in blogs)
+            {
+                blog.LastUpdated = date;
+            }
+
+            context.SaveChanges();
+        }
+
+        private class Config : ManualConfig
+        {
+            public Config()
+            {
+                SummaryStyle =
+                    SummaryStyle.Default.WithRatioStyle(RatioStyle.Percentage);
+            }
+        }
+    }
+}
